e27b6f43b07a71e5f074f667c3acb7a304d57b79
JudoJM <151977776+JudoJM@users.noreply.github.com>
2025-07-24 21:28:06 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+(function() {
+  window._POSTHOG_REMOTE_CONFIG = window._POSTHOG_REMOTE_CONFIG || {};
+  window._POSTHOG_REMOTE_CONFIG['phc_PHfwuKc2qU4z0WBSVgFimYZhdOtTR9VClofJeSAIHLA'] = {
+    config: {"token": "phc_PHfwuKc2qU4z0WBSVgFimYZhdOtTR9VClofJeSAIHLA", "supportedCompression": ["gzip", "gzip-js"], "hasFeatureFlags": true, "captureDeadClicks": false, "capturePerformance": {"network_timing": true, "web_vitals": false, "web_vitals_allowed_metrics": null}, "autocapture_opt_out": false, "autocaptureExceptions": false, "analytics": {"endpoint": "/i/v0/e/"}, "elementsChainAsString": true, "errorTracking": {"autocaptureExceptions": false, "suppressionRules": []}, "sessionRecording": {"endpoint": "/s/", "consoleLogRecordingEnabled": true, "recorderVersion": "v2", "sampleRate": null, "minimumDurationMilliseconds": 15000, "linkedFlag": null, "networkPayloadCapture": null, "masking": null, "urlTriggers": [], "urlBlocklist": [], "eventTriggers": [], "triggerMatchType": null, "scriptConfig": null}, "heatmaps": false, "surveys": false, "defaultIdentifiedOnly": true},
+    siteApps: []
+  }
+})();
